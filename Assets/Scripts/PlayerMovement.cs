3255d14af2ed6e653ae5d9509e2147dfa99c6e4f
Darren <dmeidl15@gmail.com>
2023-10-05 10:18:10 +0100

    Created PlayerMovement script and added variables

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpHeight = 7f;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+    private bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
