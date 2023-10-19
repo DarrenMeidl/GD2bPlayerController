b14a8fdf52ee089038a5c05830ab9eaed3bca4c5
DarrenMeidl <dmeidl15@gmail.com>
2023-10-19 11:23:25 +0100

    Coded in MenuManager

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -10,14 +10,14 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField]
     private string optionsSceneName = "OptionsScene";
     // Start is called before the first frame update
-    void Start()
+    void StartGame()
     {
-        
+        SceneManager.LoadScene(gameSceneName);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OpenOptions(){
+        SceneManager.LoadScene(optionsSceneName);
+    }
+    public void QuitGame(){
+        Application.Quit();
     }
 }
